79a5ec48fda5856d500fc1bb2865162ccae5390d
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2019-07-31 06:58:32 +0200

    finished first version of poorly implemented PID

--- simulator/models/pid.py
+++ simulator/models/pid.py
@@ -1,23 +1,24 @@
 class PID:
     """ A class used to model a PID regulator """
-    def __init__(self, dt, Kp, Ti, Td, I0, D0):
+    def __init__(self, dt, Kp, Ti, Td, error0 = 0):
         # constants
         self.dt = dt
         self.Kp = [Kp]
         self.Ti = [Ti]
         self.Td = [Td]
         # state variable
-        self.I = [I0]
-        self.I1 = [I0]
-        self.D = [D0]
-        self.D1 = [D0]
+        self.error = [error0]
         # inputs
         self.PV = [None]
         self.SP = [None]
         # other
-        self.error = [None]
+        self.I = [None]
+        self.D = [None]
+        self.PG = [None]
+        self.IG = [None]
+        self.DG = [None]
+        # output
         self.MV = [None]
-        self.P = [None]
 
 
     def calculate(self, SP, PV, FWD):
@@ -27,8 +28,15 @@ class PID:
         Td = self.Td[-1]
         # error
         self.error.append(SP - PV)
+        # integral and derivative parts
+        self.I.append(self.I[-1] + self.dt*((self.error[-1] + self.error[-2])/2))
+        self.D.append((self.error[-1] - self.error[-2])/(self.dt))
         # gains
-        self.P.append(Kp*self.error[-1])
-        self.I.append(self.I1[-1] + self.dt*self.error[-1])
-        self.I1.append(self.I[-1])
+        self.PG.append(Kp*self.error[-1])
+        self.IG.append((Kp/Ti)*self.I[-1])
+        self.DG.append(Kp*Td*self.D[-1])
+        # output
+        MW = self.PG[-1] + self.IG[-1] + self.DG[-1] + FWD
+        self.MV.append(MW)
+        return MW
         
