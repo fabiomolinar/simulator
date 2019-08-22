f18c5e41fc61a624716c8ddb4817787c2a3cb6e7
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2019-08-22 22:46:06 +0200

    adding new example of limited pid

--- simulator/settings/__init__.py
+++ simulator/settings/__init__.py
@@ -3,7 +3,7 @@ import os
 simulator_settings = {
     "path_to_settings": os.path.join(os.getcwd(), "simulator", "settings"),
     "path_to_models": os.path.join(os.getcwd(), "simulator", "settings", "models"),
-    "duration": 20,
+    "duration": 80,
     "dt": 0.001,
     "show_plot": True,
     "plot_update_frequency": 1.0
